759aea45759241d224e06e591b23cf91d4d0cab6
Mati Ariaudo <98933246+matijariaudo@users.noreply.github.com>
2024-03-29 17:20:07 +1100

    Se cambió y agrego parte de las funciones de wsp

--- instance/particular/apiWSP.js
+++ instance/particular/apiWSP.js
@@ -16,4 +16,37 @@ const sendMessage=async(req,res)=>{
     }
 }
 
-export	{sendMessage}
+const getChats=async(req,res)=>{
+    const {instanceId}=req.body;
+    const wsp=new Wsp();
+    const instance=await wsp.getInstance(instanceId)
+    if(!instance){return res.status(200).json({status:"error",error:"Incorrect instanceID value."})}
+    const data=await instance.getProp()
+    if(data.session!="connect"){return res.status(200).json({status:"error",error:"The session must be connected."})}
+    const chats=await instance.getChats();
+    return res.status(200).json({status:"OK",chats})
+}
+
+const getConctacts=async(req,res)=>{
+    const {instanceId,chatId,qty}=req.body;
+    const wsp=new Wsp();
+    const instance=await wsp.getInstance(instanceId)
+    if(!instance){return res.status(200).json({status:"error",error:"Incorrect instanceID value."})}
+    const data=await instance.getProp()
+    if(data.session!="connect"){return res.status(200).json({status:"error",error:"The session must be connected."})}
+    const chat=await instance.getConctacts();
+    return res.status(200).json({status:"OK",chat})
+}
+
+const getMessages=async(req,res)=>{
+    const {instanceId,chatId,qty}=req.body;
+    const wsp=new Wsp();
+    const instance=await wsp.getInstance(instanceId)
+    if(!instance){return res.status(200).json({status:"error",error:"Incorrect instanceID value."})}
+    const data=await instance.getProp()
+    if(data.session!="connect"){return res.status(200).json({status:"error",error:"The session must be connected."})}
+    const messages=await instance.getMessages(chatId,qty);
+    return res.status(200).json({status:"OK",messages})
+}
+
+export	{sendMessage,getChats,getConctacts,getMessages}
--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { Wsp } from "./waSesion.js"
-import { sendMessage } from "./instance/particular/apiWSP.js";
+import { getChats, getConctacts, getMessages, sendMessage } from "./instance/particular/apiWSP.js";
 import { showDataCpu } from "./apiCPU.js";
 import { getInfoInstances } from "./instance/general/status.js";
 import { InitWA } from "./WAAdmin.js";
@@ -15,12 +15,13 @@ router.post('/instances/start',(req,res)=>{
     res.status(200).json({status:"OK"})
 });
 
+//Particular instancias
 router.post('/instance/send',sendMessage);
-router.post('/send',sendMessage);
-router.post('/send',sendMessage);
-router.post('/send',sendMessage);
+router.post('/instance/chats',getChats);
+router.post('/instance/messages',getMessages);
+router.post('/instance/contact',getConctacts);
 
-
+//Control de consumo
 router.post('/control/cpu',showDataCpu);
 
 export {router}
